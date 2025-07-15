25b017fc8e44ff16cbbd5f0f3e1d86be713f1f02
Jerry Wen <jerrywen2005@gmail.com>
2025-07-15 18:09:46 +0000

    added auth router to main, recreated angular file structure using ng new rather than manually, created a dockerfile in root for deployment

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -24,7 +24,6 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
 def login(user: UserLogin, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.email == user.email).first()
     
-
     if not db_user or not verify_password(user.password, cast(str, db_user.hashed_password)):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     token = create_access_token({"user_id": db_user.id, "email": db_user.email})
