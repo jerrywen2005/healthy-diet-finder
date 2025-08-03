5b2f22e29637f579db2a7db452e26577c54cd99d
Jerry Wen <jerrywen2005@gmail.com>
2025-08-03 06:03:34 +0000

    Frontend: Add authGuard to restaurant page

--- frontend/src/app/restaurant/restaurant.routes.ts
+++ frontend/src/app/restaurant/restaurant.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { RestaurantInputComponent } from './restaurant-input/restaurant-input.component';
 import { RestaurantOutputComponent } from './restaurant-output/restaurant-output.component';
+import { authGuard } from '../auth/auth.guard';
 
 export const restaurantRoutes: Routes = [
-  { path: 'restaurant-input', component: RestaurantInputComponent },
-  { path: 'restaurant-output', component: RestaurantOutputComponent },
+  { 
+    path: 'restaurant-input', 
+    component: RestaurantInputComponent, 
+    canActivate: [authGuard] 
+  },
+  { 
+    path: 'restaurant-output', 
+    component: RestaurantOutputComponent, 
+    canActivate: [authGuard] 
+  },
 ];
